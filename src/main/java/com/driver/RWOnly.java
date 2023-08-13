d274b76d2610bc5139ca73f95e54fe98425c7f7a
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2023-08-13 08:07:42 +0530

    initial commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public RWOnly() {
+    }
+//
+//    public RWOnly(String name) {
+//        this.name = name;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "RWOnly{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
